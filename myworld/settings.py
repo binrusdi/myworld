406b5265180c63a08fd49780d4c3cffabfbd8e8d
binrusdi <mrusdiana07@gmail.com>
2024-04-16 22:18:54 +0700

    perubahan template dan 404

--- myworld/settings.py
+++ myworld/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ar01d=%9^p4jwyn#^=+u*tx0ypurw3i_&ta26&$%*=ttn*h1r^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
